96d36a9bb3f5c8811308af80e4cfbe289ef25089
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 09:26:57 +0700

    Dispatchers & Selectors Product screen

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState } from "react"
-import { Box, Center, Wrap, WrapItem } from "@chakra-ui/react"
-import axios from 'axios';
-import ProductCard from "../components/ProductCard"
+import { Box, Wrap, WrapItem, Center } from '@chakra-ui/react';
+import ProductCard from '../components/ProductCard';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts } from '../redux/actions/productActions';
 
-const ProductScreen = () => {
-    const [data, setData] = useState([]);
+const ProductsScreen = () => {
+	const dispatch = useDispatch();
+	const { loading, products } = useSelector((state) => state.product);
 
-    useEffect(() => {
-        axios.get('/api/products')
-             .then((response) => {
-                setData(response.data.products)
-             })
-             .catch((error) => {
-                console.error('Error Fetching Data:', error);
-             });
-    }, []);
-    return (
-        <>
-			{data.length > 1 && (
-                <Box>
-                    <Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32' }}>
-                    {data.map((product) => (
-                                <WrapItem key={product._id}>
-                                    <Center w='250px' h='450px'>
-                                        <ProductCard product={product} loading={false} />
-                                    </Center>
-                                </WrapItem>
-                            ))}
-                    </Wrap>
-                </Box>
-            )}
-        </>
+	useEffect(() => {
+		dispatch(getProducts());
+	}, [dispatch]);
 
-    )
-}
+	return (
+		<>
+			{products.length > 1 && (
+				<Box>
+					<Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32' }}>
+						{products.map((product) => (
+							<WrapItem key={product._id}>
+								<Center w='250px' h='450px'>
+									<ProductCard product={product} loading={loading} />
+								</Center>
+							</WrapItem>
+						))}
+					</Wrap>
+				</Box>
+			)}
+		</>
+	);
+};
 
-export default ProductScreen
+export default ProductsScreen;
