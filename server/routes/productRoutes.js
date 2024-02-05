6f9704781481e6797bb55345866cbef340692dcc
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 09:40:53 +0700

    Get Products Refinement To Allow Pagination

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,18 +1,26 @@
-import express from "express";
-import Product from "../models/Products.js";
+import express from 'express';
+import Product from '../models/Product.js';
 
 const productRoutes = express.Router();
 
 const getProducts = async (req, res) => {
+	const page = parseInt(req.params.page); // 1, 2 or 3
+	const perPage = parseInt(req.params.perPage); // 10
 
-    const products = await Product.find({});
+	const products = await Product.find({});
 
-    res.json({
-        products,
-        pagination: {},
-    })
+	if (page && perPage) {
+		const totalPages = Math.ceil(products.length / perPage);
+		const startIndex = (page - 1) * perPage;
+		const endIndex = startIndex + perPage;
+		const paginatedProducts = products.slice(startIndex, endIndex);
+		res.json({ products: paginatedProducts, pagination: { currentPage: page, totalPages } });
+	} else {
+		res.json({ products, pagination: {} });
+	}
 };
 
+productRoutes.route('/:page/:perPage').get(getProducts);
 productRoutes.route('/').get(getProducts);
 
 export default productRoutes;
