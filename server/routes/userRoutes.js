03eac77a1f8860b7f70e2088bee4533b83d5dbc8
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 13:43:28 +0700

    loginUser User routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import User from '../models/User.js';
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+
+const userRoutes = express.Router();
+
+//TODO: redefine expiresIn
+const genToken = (id) => {
+	return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: '60d' });
+};
+
+// login
+const loginUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPasswords(password))) {
+		user.firstLogin = false;
+		await user.save();
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			googleImage: user.googleImage,
+			goodleId: user.googleId,
+			isAdmin: user.isAdmin,
+			token: genToken(user._id),
+			active: user.active,
+			firstLogin: user.firstLogin,
+			created: user.createdAt,
+		});
+	} else {
+		res.status(401).send('Invalid Email or Password.');
+		throw new Error('User not found.');
+	}
+});
+
+userRoutes.route('/login').post(loginUser);
+
+export default userRoutes;
