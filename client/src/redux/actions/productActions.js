111e5be6d1d80dd8ab4ca1fa1249868ebe461866
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 09:52:18 +0700

    Product Actions add more function

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-import { setProducts, setLoading, setError, setPagination } from '../slices/product';
+import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle } from '../slices/product';
 import axios from 'axios';
 
 export const getProducts = (page, favouriteToggle) => async (dispatch) => {
@@ -18,5 +18,40 @@ export const getProducts = (page, favouriteToggle) => async (dispatch) => {
 					: 'An expected error has occured. Please try again later.'
 			)
 		);
+	}
+};
+
+export const addToFavorites = (id) => async (dispatch, getState) => {
+	const {
+		product: { favorites },
+	} = getState();
+
+	const newFavorites = [...favorites, id];
+	localStorage.setItem('favorites', JSON.stringify(newFavorites));
+	dispatch(setFavorites(newFavorites));
+};
+
+export const removeFromFavorites = (id) => async (dispatch, getState) => {
+	const {
+		product: { favorites },
+	} = getState();
+
+	const newFavorites = favorites.filter((favoriteId) => favoriteId !== id);
+	localStorage.setItem('favorites', JSON.stringify(newFavorites));
+	dispatch(setFavorites(newFavorites));
+};
+
+export const toggleFavorites = (toggle) => async (dispatch, getState) => {
+	const {
+		product: { favorites, products },
+	} = getState();
+
+	if (toggle) {
+		const filteredProducts = products.filter((product) => favorites.includes(product._id));
+		dispatch(setFavoritesToggle(toggle));
+		dispatch(setProducts(filteredProducts));
+	} else {
+		dispatch(setFavoritesToggle(false));
+		dispatch(getProducts(1));
 	}
 };
