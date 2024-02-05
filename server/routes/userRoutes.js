26670473331ff43b5fa669ff6947e41f055d6bad
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 13:50:18 +0700

    Register User

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -36,6 +36,46 @@ const loginUser = asyncHandler(async (req, res) => {
 	}
 });
 
+// register
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400).send('We already have an account with that email address.');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+
+	const newToken = genToken(user._id);
+
+	sendVerificationEmail(newToken, email, name);
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			googleImage: user.googleImage,
+			googleId: user.googleId,
+			firstLogin: user.firstLogin,
+			isAdmin: user.isAdmin,
+			token: newToken,
+			active: user.active,
+			createdAt: user.createdAt,
+		});
+	} else {
+		res.status(400).send('We could not register you.');
+		throw new Error('Something went wrong. Please check your information and try again.');
+	}
+});
+
+
 userRoutes.route('/login').post(loginUser);
+userRoutes.route('/register').post(registerUser);
 
 export default userRoutes;
