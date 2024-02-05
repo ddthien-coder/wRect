dbb59946a78ac6750124a26450b747dd879e4c33
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 10:53:57 +0700

    NavLink Component

---
+++ client/src/components/NavLink.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { IconButton } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+
+const NavLink = ({ children, route }) => (
+	<IconButton as={ReactLink} px='2' py='1' rounded='md' variant='ghost' to={route}>
+		{children}
+	</IconButton>
+);
+
+export default NavLink;
