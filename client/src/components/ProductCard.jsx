a29a60b24d6fd41b5e9e0e7f0a1afccf045039ad
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-05 10:07:02 +0700

    Product Card Add & Remove Favorite

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import { Box, Image, Text, Badge, Flex, IconButton, Skeleton } from "@chakra-ui/react";
-import {BiExpand} from 'react-icons/bi';
+import { Box, Image, Text, Badge, Flex, IconButton, Skeleton } from '@chakra-ui/react';
+import { BiExpand } from 'react-icons/bi';
+import React from 'react';
+import { addToFavorites, removeFromFavorites } from '../redux/actions/productActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdOutlineFavorite, MdOutlineFavoriteBorder } from 'react-icons/md';
 
-const ProductCard = ({product, loading}) => {
-  return (
-<Skeleton isLoaded={!loading} _hover={{ size: 1.5 }}>
+const ProductCard = ({ product, loading }) => {
+	const dispatch = useDispatch();
+	const { favorites } = useSelector((state) => state.product);
+
+	return (
+		<Skeleton isLoaded={!loading} _hover={{ size: 1.5 }}>
 			<Box
 				_hover={{ transform: 'scale(1.1)', transitionDuration: '0.5s' }}
 				borderWidth='1px'
@@ -41,9 +47,28 @@ const ProductCard = ({product, loading}) => {
 						${product.price}
 					</Text>
 				</Flex>
-				<IconButton icon={<BiExpand size='20' />} colorScheme='cyan' size='sm' />
+				<Flex justify='space-between' mt='2'>
+					{favorites.includes(product._id) ? (
+						<IconButton
+							icon={<MdOutlineFavorite size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(removeFromFavorites(product._id))}
+						/>
+					) : (
+						<IconButton
+							icon={<MdOutlineFavoriteBorder size='20px' />}
+							colorScheme='cyan'
+							size='sm'
+							onClick={() => dispatch(addToFavorites(product._id))}
+						/>
+					)}
+
+					<IconButton icon={<BiExpand size='20' />} colorScheme='cyan' size='sm' />
+				</Flex>
 			</Box>
 		</Skeleton>
-  )
-}
-export default ProductCard
+	);
+};
+
+export default ProductCard;
