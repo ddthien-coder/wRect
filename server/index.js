d8fde548871dc1c016483056da6abceb462b085c
ddthien-coder <jungjihoonkr97@gmail.com>
2024-02-06 14:25:40 +0700

    Preparing

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import connectDb from './db.js';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 //Routes
 import productRoutes from './routes/productRoutes.js';
@@ -20,6 +21,15 @@ app.get('/api/config/google', (req, res) => res.send(process.env.GOOGLE_CLIENT_I
 
 
 const port = 5000;
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.get('/',(req, res) => {
     res.send('API is running...')
